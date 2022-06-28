efdb86e7bb0699c3bb65956d51798ba67e3f29a4
LucaParenti <luca.parenti1@studenti.unimi.it>
2022-06-28 21:36:59 +0200

    Refactoring

--- src/public/js/helpers/theme.js
+++ src/public/js/helpers/theme.js
@@ -16,25 +16,25 @@ function checkTheme () {
 // @author Luca Parenti <luca.parenti1@studenti.unimi.it>
 function activateDarkMode () {
   // Replace all the instance in the file from ligth to dark mode
-  document.querySelectorAll(".bg-light").forEach((element) => {
-    element.className = element.className.replace(/-light/g, "-dark");
+  document.querySelectorAll('.bg-light').forEach((element) => {
+    element.className = element.className.replace(/-light/g, '-dark');
   });
 
   // Add dark background to body
-  document.body.classList.add("bg-dark");
+  document.body.classList.add('bg-dark');
 
   // Replace all the light text to dark text if any
-  if (document.body.classList.contains("text-dark")) {
-    document.body.classList.replace("text-dark", "text-light");
+  if (document.body.classList.contains('text-dark')) {
+    document.body.classList.replace('text-dark', 'text-light');
   } else {
-    document.body.classList.add("text-light");
+    document.body.classList.add('text-light');
   }
 
   // Change the login and signup buttons
   var loginButton = document.getElementById('login-button');
   var signupButton = document.getElementById('signup-button');
-  loginButton.classList.replace("btn-outline-dark", "btn-outline-light");
-  signupButton.classList.replace("btn-outline-dark", "btn-outline-light");
+  loginButton.classList.replace('btn-outline-dark', 'btn-outline-light');
+  signupButton.classList.replace('btn-outline-dark', 'btn-outline-light');
 
   // Save the theme to localstorage
   const theme = {
@@ -42,46 +42,46 @@ function activateDarkMode () {
     date: new Date().getTime()
   }
 
-  localStorage.setItem("Theme", JSON.stringify(theme));
+  localStorage.setItem('Theme', JSON.stringify(theme));
 }
 
 // This function activate the light mode
 // @author Luca Parenti <luca.parenti1@studenti.unimi.it>
 function activateLightMode () {
   // Replace all the instance in the file from dark to light mode
-  document.querySelectorAll(".bg-dark").forEach((element) => {
-    element.className = element.className.replace(/-dark/g, "-light");
+  document.querySelectorAll('.bg-dark').forEach((element) => {
+    element.className = element.className.replace(/-dark/g, '-light');
   });
 
   // Add light background to body
-  document.body.classList.add("bg-light");
+  document.body.classList.add('bg-light');
 
   // Replace all the dark text to ligth text if any
-  if (document.body.classList.contains("text-light")) {
-    document.body.classList.replace("text-light", "text-dark");
+  if (document.body.classList.contains('text-light')) {
+    document.body.classList.replace('text-light', 'text-dark');
   } else {
-    document.body.classList.add("text-dark");
+    document.body.classList.add('text-dark');
   }
 
   // Change the login and signup buttons
   var loginButton = document.getElementById('login-button');
   var signupButton = document.getElementById('signup-button');
-  loginButton.classList.replace("btn-outline-light", "btn-outline-dark");
-  signupButton.classList.replace("btn-outline-light", "btn-outline-dark");
+  loginButton.classList.replace('btn-outline-light', 'btn-outline-dark');
+  signupButton.classList.replace('btn-outline-light', 'btn-outline-dark');
 
   // Save the theme to localstorage
   const theme = {
-    ligthTheme: true,
+    darkTheme: false,
     date: new Date().getTime()
   }
 
-  localStorage.setItem("Theme", JSON.stringify(theme));
+  localStorage.setItem('Theme', JSON.stringify(theme));
 }
 
 // This function load the theme from localstorage when pages loads
 // @author Luca Parenti <luca.parenti1@studenti.unimi.it>
 function loadTheme () {
-  var theme = localStorage.getItem("Theme");
+  var theme = localStorage.getItem('Theme');
 
   if (theme != null) {
     theme = JSON.parse(theme);
