154a75ac27da542d9ec9a6653326be4085a654ab
LucaParenti <luca.parenti1@studenti.unimi.it>
2022-06-28 21:36:41 +0200

    Added function to get forecast details from homepage

--- src/public/js/homepage.js
+++ src/public/js/homepage.js
@@ -23,11 +23,38 @@ async function getUserWeatherInfo (lat, lon) {
   const weatherResponse = await validator(`/api/v1/getPositionalWeather/${lat}/${lon}`);
   document.getElementById('user-position').innerText = 'Posizione rilevata: ' + weatherResponse.name;
   document.getElementById('user-temp').innerText = 'Temperatura: ' + weatherResponse.main.temp + '°C';
+
+  // Save the city to localstorage
+  const city = {
+    cityName: weatherResponse.name,
+    date: new Date().getTime()
+  };
+
+  localStorage.setItem('City', JSON.stringify(city));
+}
+
+// Function that loads details of the city directly from the homepage
+// @author Luca Parenti <luca.parenti1@studenti.unimi.it>
+function loadDetails () {
+  var city = localStorage.getItem('City');
+  city = JSON.parse(city);
+  const cityName = city.cityName;
+  window.location.href = '/cities?cityName=' + cityName;
 }
 
 // Made the entire weather load whenever a user get in the homepage
 // @author Luca Parenti <luca.parenti1@studenti.unimi.it>
 document.addEventListener('DOMContentLoaded', () => {
+  // Clear localstorage after some time
+  var city = localStorage.getItem('City');
+  city = JSON.parse(city);
+  var date = city.date;
+  var today = new Date().getTime();
+
+  if (today - date > (1000 * 86400)) {
+    localStorage.clear()
+  }
+
   navigator.geolocation.getCurrentPosition((position) => {
     const { latitude, longitude } = position.coords;
     getUserWeatherInfo(latitude, longitude);
